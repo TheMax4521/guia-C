a9a50f17632570a12266d05e3c4d930266bc6db2
dario <dariorossel@hotmail.com>
2025-04-11 20:31:38 -0300

    intentando

--- estructuras.c
+++ estructuras.c
@@ -6,5 +6,31 @@ int main(){
     {
        printf("i = %d\n", i);
     }
+
+    int a[] = {1, 2, 3, 4};
+    int n = sizeof(a)/sizeof(a[0]);
+    int b[n];
+    printf("arreglo original:");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    
+    printf("arreglo modificado:");
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i - 1 < 0)
+        {
+            b[n - 1] = a[i];
+        } else{
+            b[i-1] = a[i];
+        }
+     printf("%d ", b[i]);   
+    }
+    
+    
+
     
 }
