eaa63391734618fa13c704005ad864b4e771571a
dario <dariorossel@hotmail.com>
2025-04-11 20:49:19 -0300

    avance

--- estructuras.c
+++ estructuras.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 int main(){
     for (int i = 10; i > 0; i--)
@@ -7,9 +8,10 @@ int main(){
        printf("i = %d\n", i);
     }
 
-    int a[] = {1, 2, 3, 4};
+    int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int n = sizeof(a)/sizeof(a[0]);
     int b[n];
+    int k = 5;
     printf("arreglo original:");
     for (int i = 0; i < n; i++)
     {
@@ -21,16 +23,35 @@ int main(){
 
     for (int i = 0; i < n; i++)
     {
-        if (i - 1 < 0)
+        if (i - k < 0)
         {
-            b[n - 1] = a[i];
+            b[n + (i - k)] = a[i];
         } else{
-            b[i-1] = a[i];
+            b[i - k] = a[i];
         }
-     printf("%d ", b[i]);   
     }
     
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", b[i]);
+    }
     
+    printf("\n");
 
-    
+    int veces[] = {0, 0, 0, 0, 0, 0};
+    int cantidad_de_tiradas = 100;
+    int longitud = sizeof(veces)/sizeof(veces[0]);
+
+    for (int i = 0; i < cantidad_de_tiradas; i++)
+    {
+        int dado = (rand() % 6);
+        veces[dado] = veces[dado] + 1;
+    }
+
+    for (int i = 0; i < longitud; i++)
+    {
+        printf("%d ", veces[i]);
+    }
+    printf("\n");
+
 }
