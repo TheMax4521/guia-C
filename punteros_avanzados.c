f39f3469422eef9e538a3a6200e50879b45b7aef
dario <dariorossel@hotmail.com>
2025-04-18 19:26:13 -0300

    no entiendo

--- punteros_avanzados.c
+++ punteros_avanzados.c
@@ -1,15 +1,43 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct{
+    char nombre[100];
+    int edad;
+}persona_t;
+
+persona_t* crear_persona(const char* nombre, int edad){
+    persona_t* nueva = malloc(sizeof(persona_t));
+
+    if (nueva == NULL)
+    {
+        return NULL;
+    }
+    
+    strcpy(nueva -> nombre, nombre);
+    nueva -> edad = edad;
+    return nueva;
+
+}
 
 int main(){
-    char palabra[100];
-    char *p = palabra;
-    printf("Ingrese string:");
-    scanf("%s", palabra);
-    for (int i = 0; *p != '\0'; i++)
+    persona_t* pr = crear_persona("Pepe", 23);
+    if (pr != NULL)
     {
-        palabra[i] = toupper(*p);
+        printf("Nombre: %s\n", pr -> nombre);
+        printf("Edad: %d\n", pr -> edad);
+    }
+    free(pr);
+
+    char palabra[100];
+    printf("Ingrese string:");
+    fgets(palabra, sizeof(palabra), stdin);
+    for (char *p = palabra; *p != '\0'; p++)
+    {
+        *p = toupper(*p);
     }
     printf("%s", palabra);
     printf("\n");
